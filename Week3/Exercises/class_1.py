b19cbc5604a326ab7ac9ba05fb76d1bc0019ea96
Yehor Dudnik <yehor.dudnik@gmail.com>
2023-09-20 20:22:18 +0100

    working with classes

---
+++ Week3/Exercises/class_1.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Make: {self.make}, Model: {self.model}, Year: {self.year}")
+
+
+car1 = Car("Toyota", "Camry", 2022)
+
+car1.display_info()
